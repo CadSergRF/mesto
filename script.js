d2ea59b2b6454e170e057310eb38d773d9e24cee
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-11-10 13:15:46 +0700

    Added popup

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let like = document.querySelector('.place__like');
-like.addEventListener('click', function () {
-    like.classList.toggle('place__like_active');
+let placeLike = document.querySelector('.place__like');
+placeLike.addEventListener('click', function () {
+    placeLike.classList.toggle('place__like_active');
 })
+
+let userProfileEdit = document.querySelector('.user-profile__edit');
+let popupCloseBtn = document.querySelector('.popup__close-btn');
+let popupSave = document.querySelector('.popup__save');
+
+function openPopup() {
+    let openPopup = document.querySelector('.popup');
+    openPopup.classList.add('popup_opened');
+}
+function closePopup() {
+    let close = document.querySelector('.popup');
+    close.classList.remove('popup_opened');
+}
+
+userProfileEdit.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
+popupSave.addEventListener('click', closePopup);
