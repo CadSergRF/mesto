e24bd99315c788c2fa5b207f838cf90d3d79b578
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-11-17 15:54:49 +0700

    hotfix/header

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 let userProfileEdit = document.querySelector('.user-profile__edit');
 let popupCloseBtn = document.querySelector('.popup__close-btn');
 let formEditProfile = document.getElementById('popup__form');
-let popupName = document.querySelector('.popup__input_name');
+let popupName = document.querySelector('.popup__input_editProfile_name');
 let userName = document.querySelector('.user-profile__name');
-let popupJob = document.querySelector('.popup__input_job');
+let popupJob = document.querySelector('.popup__input_editProfile_job');
 let userJob = document.querySelector('.user-profile__job');
 
 
