3df5585d6a0e7b8a0fb27e535c9dfac3a7455e70
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-11-17 14:58:45 +0700

    hotfix script

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,17 @@
 let userProfileEdit = document.querySelector('.user-profile__edit');
 let popupCloseBtn = document.querySelector('.popup__close-btn');
 let formEditProfile = document.getElementById('popup__form');
+let popupName = document.querySelector('.popup__input_name');
+let userName = document.querySelector('.user-profile__name');
+let popupJob = document.querySelector('.popup__input_job');
+let userJob = document.querySelector('.user-profile__job');
 
-//Фунция отображения лайков
-function likes() {
-    let likeIcon = document.querySelectorAll('.place__like');
-    for (let i = 0; i < likeIcon.length; i++) {
-        likeIcon[i].addEventListener('click', function () {
-            likeIcon[i].classList.toggle('place__like_active');
-        })
-    }
-};
 
 // Открытие и закрытие PopUp
 function openPopup() {
-    let openPopup = document.querySelector('.popup');
-    let popupName = document.querySelector('.popup__input_name');
-    let userName = document.querySelector('.user-profile__name');
-    let popupJob = document.querySelector('.popup__input_job');
-    let userJob = document.querySelector('.user-profile__job');
-    
+    let openPopup = document.querySelector('.popup'); 
+    // если переменную openPopup вынести из функии то код перестает работать. Непонимаю почему.
+
     openPopup.classList.add('popup_opened');
     // Заполняем поля текущим значением
     popupName.value = userName.textContent;
@@ -33,10 +25,6 @@ function closePopup() {
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let popupName = document.querySelector('.popup__input_name');
-    let userName = document.querySelector('.user-profile__name');
-    let popupJob = document.querySelector('.popup__input_job');
-    let userJob = document.querySelector('.user-profile__job');
 
     userName.textContent = popupName.value;
     userJob.textContent = popupJob.value;
@@ -45,7 +33,6 @@ function formSubmitHandler(evt) {
 }
 
 
-likes();
 formEditProfile.addEventListener('submit', formSubmitHandler);
 userProfileEdit.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
