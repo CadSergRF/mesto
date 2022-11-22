9efaf654a5ea90947e58d082bcc7f3268515fbd0
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-11-22 16:52:35 +0700

    feat: make place template builder

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,29 @@
-const popup = document.querySelector('.popup');
+const popupElement = document.querySelector('.popup');
 const formEditProfile = document.getElementById('popup__form');
 const popupCloseBtn = document.querySelector('.popup__close-btn');
-const popupName = document.querySelector('.popup__input_edit_name');
-const popupJob = document.querySelector('.popup__input_edit_job');
+const popupEditName = document.querySelector('.popup__input_edit_name');
+const popupEditJob = document.querySelector('.popup__input_edit_job');
 const userProfileEdit = document.querySelector('.user-profile__edit');
 const userName = document.querySelector('.user-profile__name');
 const userJob = document.querySelector('.user-profile__job');
 
-
 // Открытие и закрытие PopUp
 function openPopup() {
-  popup.classList.add('popup_opened');
+  popupElement.classList.add('popup_opened');
   // Заполняем поля текущим значением
-  popupName.value = userName.textContent;
-  popupJob.value = userJob.textContent;
+  popupEditName.value = userName.textContent;
+  popupEditJob.value = userJob.textContent;
 };
 
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  popupElement.classList.remove('popup_opened');
 };
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  userName.textContent = popupName.value;
-  userJob.textContent = popupJob.value;
+  userName.textContent = popupEditName.value;
+  userJob.textContent = popupEditJob.value;
 
   closePopup();
 }
---
+++ scripts/placeTemplateBuilder.js
@@ -0,0 +1,23 @@
+/* Создаем шаблон карточки place как элемент списка */
+/* Объявляем переменные и присваиваем классы */
+const footerTag = document.querySelector('footer');
+const placeTemplateElement = document.createElement('template');
+placeTemplateElement.id = 'placeTemplate';
+const placesItemElement = document.createElement('li');
+placesItemElement.classList.add('places__item');
+const placeElement = document.createElement('article');
+placeElement.classList.add('place');
+const placeImageElement = document.createElement('img');
+placeImageElement.classList.add('place__image');
+const placeInfoElement = document.createElement('div');
+placeInfoElement.classList.add('place__info');
+const placeTitleElement = document.createElement('h2');
+placeTitleElement.classList.add('place__title');
+const placeLikeElement = document.createElement('button');
+placeLikeElement.classList.add('place__like');
+/* Собираем шаблон */
+footerTag.after(placeTemplateElement);
+placeTemplateElement.append(placesItemElement);
+placesItemElement.append(placeElement);
+placeElement.append(placeImageElement, placeInfoElement);
+placeInfoElement.append(placeTitleElement, placeLikeElement);
