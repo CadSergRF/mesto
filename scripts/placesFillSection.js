fd74327e51c5ca98153cecc95ac26cb2e56f997e
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-11-22 21:43:51 +0700

    feat: places fill from base

---
+++ scripts/placesFillSection.js
@@ -0,0 +1,42 @@
+const initialPlaces = [
+  {
+    name: 'Карачаевск. Сентинский храм',
+    link: './img/content/Karachaevsk.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './img/content/gora-elbrus.jpg'
+  },
+  {
+    name: 'Гора Белалакая',
+    link: './img/content/dombaj.jpg'
+  },
+  {
+    name: 'Перевал Кату-ярык',
+    link: './img/content/pereval-katu-yarik.jpg'
+  },
+  {
+    name: 'Телецкое озеро',
+    link: './img/content/ozero-teleckoe.jpg'
+  },
+  {
+    name: 'Шерегеш. Гора Зеленая',
+    link: './img/content/sheregesh-gora-zelenaya.jpg'
+  },
+]
+
+const placesListElement = document.querySelector('.places__list');
+
+function addPlace(name, link) {
+  const placeTemplateElement = document.querySelector('#placeTemplate').content;
+  const placeElement = placeTemplateElement.querySelector('.places__item').cloneNode(true);
+
+  placeElement.querySelector('.place__image').src = link;
+  placeElement.querySelector('.place__image').alt = name;
+  placeElement.querySelector('.place__title').textContent = name;
+
+  placesListElement.prepend(placeElement);
+}
+
+/* Заполняем контентом */
+initialPlaces.map(elem => addPlace(elem.name, elem.link));
