8b42c26c186c06ca17e169b4a41e2647acece922
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-11-22 10:49:17 +0700

    feat: content places into grid again

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,32 @@
-let userProfileEdit = document.querySelector('.user-profile__edit');
-let popupCloseBtn = document.querySelector('.popup__close-btn');
-let formEditProfile = document.getElementById('popup__form');
-let popupName = document.querySelector('.popup__input_edit_name');
-let userName = document.querySelector('.user-profile__name');
-let popupJob = document.querySelector('.popup__input_edit_job');
-let userJob = document.querySelector('.user-profile__job');
-let popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
+const formEditProfile = document.getElementById('popup__form');
+const popupCloseBtn = document.querySelector('.popup__close-btn');
+const popupName = document.querySelector('.popup__input_edit_name');
+const popupJob = document.querySelector('.popup__input_edit_job');
+const userProfileEdit = document.querySelector('.user-profile__edit');
+const userName = document.querySelector('.user-profile__name');
+const userJob = document.querySelector('.user-profile__job');
+
 
 // Открытие и закрытие PopUp
 function openPopup() {
-    popup.classList.add('popup_opened');
-    // Заполняем поля текущим значением
-    popupName.value = userName.textContent;
-    popupJob.value = userJob.textContent;
+  popup.classList.add('popup_opened');
+  // Заполняем поля текущим значением
+  popupName.value = userName.textContent;
+  popupJob.value = userJob.textContent;
 };
 
 function closePopup() {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 };
 
 function formSubmitHandler(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    userName.textContent = popupName.value;
-    userJob.textContent = popupJob.value;
+  userName.textContent = popupName.value;
+  userJob.textContent = popupJob.value;
 
-    closePopup();
+  closePopup();
 }
 
 
