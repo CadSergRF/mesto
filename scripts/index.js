095e4e167e8a1599740a378dab3d1f83084dc59e
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-11-25 08:52:09 +0700

    refactor: вынести пепеменные начальной базы в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,4 @@
 /* Переменные */
-const initialPlaces = [  // Начальная база places
-  {
-    name: 'Кузнецкий Алатау',
-    link: './img/content/kuzneckij-alatau.jpg'
-  },
-  {
-    name: 'Озеро Байкал',
-    link: './img/content/ozero-baykal.jpg'
-  },
-  {
-    name: 'Красноярские столбы',
-    link: './img/content/krasnoyarskie-stolbi.jpg'
-  },
-  {
-    name: 'Перевал Кату-ярык',
-    link: './img/content/pereval-katu-yarik.jpg'
-  },
-  {
-    name: 'Телецкое озеро',
-    link: './img/content/ozero-teleckoe.jpg'
-  },
-  {
-    name: 'Шерегеш. Гора Зеленая',
-    link: './img/content/sheregesh-gora-zelenaya.jpg'
-  },
-]
-
 const editProfileForm = document.forms.editProfile;   // Форма редактирования профиля
 const addPlaceForm = document.forms.addPlace;         // Форма добавления place
 const popup = document.querySelector('.popup');
