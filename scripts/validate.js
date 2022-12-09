5045f226124d9b1a1775b77e6dca7977abcc1f36
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-12-09 21:39:47 +0700

    feat: сделать валидацию + стандартные сообщения ошибок

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,37 +1,43 @@
 import { configValidation } from './configValidation.js';
 
+function showErrorMessage(form, input, config, errorMessage) {
+  const errorElem = form.querySelector(`.${input.name}-error`);
+  input.classList.add(`${config.inputErrorClass}`);
+  errorElem.textContent = errorMessage;
+};
 
+function hideErrorMessage(form, input, config) {
+  const errorElem = form.querySelector(`.${input.name}-error`);
+  input.classList.remove(`${config.inputErrorClass}`);
+  errorElem.textContent = '';
+}
 
+function checkValidityForm(form, inputs, config) { // проверка валидации
+  inputs.forEach((input) => {
+    input.addEventListener('input', () => {
+      if (!input.validity.valid) {
+        showErrorMessage(form, input, config, input.validationMessage);
+      } else {
+        hideErrorMessage(form, input, config);
+      }
+    });
+  });
+};
 
 function enableValidation(config) {
   const { formSelector, inputSelector, submitButtonSelector, ...errorConfig } = config;
-
   const forms = document.querySelectorAll(formSelector);
 
   forms.forEach((form) => {
-    const inputs = form.querySelectorAll(inputSelector);
+    const inputs = [...form.querySelectorAll(inputSelector)];
     const button = form.querySelector(submitButtonSelector);
 
     form.addEventListener('submit', (evt) => {
       evt.preventDefault();
     })
 
-    inputs.forEach ((input) => {
-      // Функция проверки валидации
-      if (!input.validity.valid) {
-        const errorElem = form.querySelector(`.${input.name}-error`);
-        input.classList.add(`${errorConfig.inputErrorClass}`);
-        errorElem.textContent = 'Ошибка';
-
-      } else {
-        const errorElem = form.querySelector(`.${input.name}-error`);
-        input.classList.remove(`${errorConfig.inputErrorClass}`);
-        errorElem.textContent = '';
-
-      }
-    })
+    checkValidityForm(form, inputs, errorConfig); //Валидация
   })
-
 };
 
 
