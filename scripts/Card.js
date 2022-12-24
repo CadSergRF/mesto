c24d6ca853c316830626b960cb60106e3c4cca1d
Sergey Evdokimov <cadsergrf@yandex.ru>
2022-12-24 12:17:07 +0700

    pref: class Card

--- scripts/Card.js
+++ scripts/Card.js
@@ -15,4 +15,23 @@ class Card {
 
     return cardElement;
   }
+
+  _addListeners() {
+
+  }
+
+  createCard() {
+    this._element = this._getTemplate();
+    const cardImage = this._cardElement.querySelector('.place_image');
+    const cardLike = this._element.querySelector('.place__like');
+    const cardDelete = this._element.querySelector('.place__delete');
+    const cardFull = this._element.querySelector('.place');
+
+    cardImage.src = this._image;
+    cardImage.alt = this._description;
+    cardElement.querySelector('.place__title').textContent = this._name;
+    this._addListeners(cardLike, cardDelete, cardFull);
+
+    return this._element;
+  }
 }
